0de4be6806ddc3b9bf91ea091dc367f4e4513581
bielb1b2 <gabrielvt.souza@hotmail.com>
2022-03-25 18:29:01 -0300

    Texto adicionado

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import {
     Box,
     Flex,
-    Image
+    Image,
+    Text
 } from "@chakra-ui/react";
 
 import imgAuthHorizon from '../assets/HorizonImg.png';
@@ -10,8 +11,30 @@ import imgAuthHorizon from '../assets/HorizonImg.png';
 export default function Auth() {
     return (
         <Flex w="100%" h="100vh" overflow="hidden" position="absolute">
-            <Image src={imgAuthHorizon} w="100%" h="100%"/>
-            <Box width="100%" h="100%" position="fixed" bgGradient="linear(to-r, #282A37 30%, rgba(40, 42, 55, 0.77) 98.78%)"/>
+            <Image src={imgAuthHorizon} w="100%" h="100%" position="fixed" zIndex={-1} />
+            <Box width="100%"
+                h="100%"
+                position="fixed"
+                bgGradient="linear(to-r, #282A37 30%, rgba(40, 42, 55, 0.57) 98.78%)"
+                zIndex={0}
+            />
+            <Flex direction="column"
+                width="100%"
+                h="100%"
+                zIndex={1}
+                px={100}
+                py={100}
+            >
+                <Text fontWeight="bold" color="gray.500">Start for free</Text>
+                <Flex>
+                    <Text m="0" fontSize={44} fontWeight="bold" color="white">Create new account</Text>
+                    <Text m="0" fontSize={44} fontWeight="bold" color="blue.500">.</Text>
+                </Flex>
+                <Flex>
+                    <Text color="white">Already a member? </Text>
+                    <Text color="blue.500">&nbsp;Log In</Text>
+                </Flex>
+            </Flex>
         </Flex>
     )
 }
