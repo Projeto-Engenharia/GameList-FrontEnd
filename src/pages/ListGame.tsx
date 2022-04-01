8bce05ddc5a5cc773bb84f4789de99a59bd05fb6
bielb1b2 <gabrielvt.souza@Hotmail.com>
2022-04-01 09:55:23 -0300

    Front end version 1

--- src/pages/ListGame.tsx
+++ src/pages/ListGame.tsx
@@ -1,10 +1,42 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { Button, Flex, Text, TableContainer, Table, Thead, Tr, Th, Tbody, Td } from '@chakra-ui/react';
+import { AuthContext } from '../hooks/AuthHook';
+import api from '../api/api';
 
 export default function ListGame() {
+    const { signOut } = useContext(AuthContext)
+
+    const [listUser, setListUser] = useState<any[]>();
+
+    useMemo(async() => {
+        const response = await api.get('/users/list');
+        return setListUser(response.data);
+    }, [])
+
     return(
-        <Flex w="100%" >
-            
+        <Flex w="100%" direction="column">
+            <Text mb="10px">Usuários</Text>
+                <TableContainer>
+                    <Table variant='unstyled'>
+                        <Thead>
+                            <Tr>
+                                <Th>Nick</Th>
+                                <Th>Email</Th>
+                                <Th>Password</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {listUser?.map(user => (
+                            <Tr>
+                                <Td>{user.nick}</Td>
+                                <Td>{user.email}</Td>
+                                <Td>{user.password}</Td>
+                            </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            <Button w="100px" color="white" bgColor="red.400" _hover={{ bgColor: "red.600" }} onClick={signOut}>Logout</Button>
         </Flex>
     )
 }
